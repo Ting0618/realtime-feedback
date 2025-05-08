21f2ede47dab7e9395456018fa82789902c30482
tingwong <tingwong0704@gmail.com>
2025-05-08 20:34:23 +0100

    Initial commit

---
+++ src/main/java/com/feedback/realtime_feedback/RealtimeFeedbackApplication.java
@@ -0,0 +1,13 @@
+package com.feedback.realtime_feedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeFeedbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimeFeedbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/feedback/realtime_feedback/RealtimeFeedbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feedback.realtime_feedback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeFeedbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
